903d517e81fb251103a9b42104a786c9603e67ac
Lukasz Choros <lchoros@nstgroup.co.uk>
2023-06-06 21:31:29 +0100

    define repositories, dbContext, set connection strings

---
+++ LibraryManagement.Data/DbContexts/LibraryContext.cs
@@ -0,0 +1,13 @@
+﻿using LibraryManagement.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagement.Data.DbContexts
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors{get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<BookCopy> BookCopies { get; set; }
+    }
+}
